50bcea327c37d54139700084b30521e432427c20
Thai Doan <doannamthai123@gmail.com>
2019-04-12 17:50:24 -0600

    Updates

---
+++ precache-manifest.368e6c0ac7c0a794f8d492540f65bbbb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9edff0ef042e9feed04adbfae24e480",
+    "url": "/Card-Game-Front-End/static/media/mailholder.b9edff0e.svg"
+  },
+  {
+    "revision": "8344e602a7502bf3fc6af8390c1078c5",
+    "url": "/Card-Game-Front-End/static/media/hell.8344e602.jpg"
+  },
+  {
+    "revision": "dd39c5544a5111bea7da03ac66c5a242",
+    "url": "/Card-Game-Front-End/static/media/background1.dd39c554.jpg"
+  },
+  {
+    "revision": "7a8cc0ebf3b23eec3107abf256045b20",
+    "url": "/Card-Game-Front-End/static/media/avatar.7a8cc0eb.jpg"
+  },
+  {
+    "revision": "fb2ee75cb458c457cfb8",
+    "url": "/Card-Game-Front-End/static/js/runtime~main.fb2ee75c.js"
+  },
+  {
+    "revision": "e0790b5372dbd152c3b5",
+    "url": "/Card-Game-Front-End/static/js/main.e0790b53.chunk.js"
+  },
+  {
+    "revision": "5472b29bb2a38d864b5b",
+    "url": "/Card-Game-Front-End/static/js/2.5472b29b.chunk.js"
+  },
+  {
+    "revision": "e0790b5372dbd152c3b5",
+    "url": "/Card-Game-Front-End/static/css/main.fe34c95d.chunk.css"
+  },
+  {
+    "revision": "964560d96522cc4aaf7153aa757cc0f2",
+    "url": "/Card-Game-Front-End/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Card-Game-Front-End/precache-manifest.368e6c0ac7c0a794f8d492540f65bbbb.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Card-Game-Front-End/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
